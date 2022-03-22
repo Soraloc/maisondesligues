10b0036e8628a507e3c72a02ae996e1678fea3ef
Paulhenrisolat <paulhenrisolat@gmail.com>
2022-03-22 12:50:29 +0100

    testpage + core
    changer les doubles extends

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class TestController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="accueil")
-     * @Route("/")
+     * @Route("/test", name="test")
      */
     public function accueil(): Response
     {
-        return $this->render('vue/accueil.html.twig');
+        return $this->render('vue/test.html.twig');
     }
 }
