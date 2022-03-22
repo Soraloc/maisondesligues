f43c7f55808e3e2952dd812af5fb5205fc44b308
PGLucas <garambois.lucas@gmail.com>
2022-03-22 15:27:32 +0100

    Ajout de la création de compte

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\Compte;
 
 class SecurityController extends AbstractController
 {
@@ -23,7 +24,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('vue/security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
@@ -32,5 +33,13 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+    
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register() : Reponse
+    {
+        return $this->render('security/register.html.twig' );
     }
 }
