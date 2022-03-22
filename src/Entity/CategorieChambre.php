0e50bce732c20078b1ca7fdadbcd81164cc97d90
Paulhenrisolat <78586107+Paulhenrisolat@users.noreply.github.com>
2022-03-22 15:24:48 +0100

    Update src/Entity/CategorieChambre.php
    Co-authored-by: Nicolas PREAUX <75117320+Soraloc@users.noreply.github.com>

--- src/Entity/CategorieChambre.php
+++ src/Entity/CategorieChambre.php
@@ -19,7 +19,7 @@ class CategorieChambre
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=50, name="libellecategorie")
      */
     private $libelleCategorie;
 
