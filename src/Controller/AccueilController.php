8779559fd52430278f1a48f6217682101cfba1f4
Paulhenrisolat <paulhenrisolat@gmail.com>
2022-03-30 11:00:24 +0200

    change accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\HotelRepository;
+use App\Repository\AtelierRepository;
 
 class AccueilController extends AbstractController
 {
@@ -13,11 +14,13 @@ class AccueilController extends AbstractController
      * @Route("/accueil", name="accueil")
      * @Route("/")
      */
-    public function accueil(HotelRepository $repo): Response
+    public function accueil(HotelRepository $repoHotel, AtelierRepository $repoAtelier): Response
     {
-        $hotels = $repo->findAll();
+        $hotels = $repoHotel->findAll();
+        $ateliers = $repoAtelier->findAll();
         return $this->render('vues/accueil.html.twig', [
-            'hotels' => $hotels
+            'hotels' => $hotels,
+            'ateliers' => $ateliers
         ]);
     }
 }
