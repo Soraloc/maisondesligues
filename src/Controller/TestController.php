192f873ae2b04281a892ef292f9892d822586e0e
PGLucas <78586639+PGLucas@users.noreply.github.com>
2022-05-04 23:05:20 +0200

    Les mail fonctionne

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,25 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use ;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 
 class TestController extends AbstractController {
 
     /**
-     * @Route("/test", name="test")
-     */
-    public function accueil(): Response {
-        define('SENDGRID_API_KEY', '
-                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');
-    }
-
-    /**
      * @Route("/mail", name="mail")
      */
-    public function Mail(): Response {
+    public function Mail(MailerInterface $mailer): Response 
+    {
          $email = (new Email())
-            ->from('hello@example.com')
-            ->to('you@example.com')
+            ->from('garambois.lucas@gmail.com')
+            ->to('garambois.lucas@gmail.com')
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
@@ -33,6 +27,9 @@ class TestController extends AbstractController {
             ->html('<p>See Twig integration for better HTML integration!</p>');
 
         $mailer->send($email);
+        return new Response(
+          'Email was sent'
+       );
     }
 
 }
